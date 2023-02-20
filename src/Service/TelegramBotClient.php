2c2f1615be3bb430c2a4343a5e4d48f8459f86bc
Sergei Pashkevich <pashkevich.s.d@gmail.com>
2023-02-20 21:04:09 +0300

    added send document

--- src/Service/TelegramBotClient.php
+++ src/Service/TelegramBotClient.php
@@ -5,12 +5,15 @@ namespace Pashkevichsd\TelegramBotClient\Service;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\Multipart\FormDataPart;
 
 class TelegramBotClient
 {
     private const API = 'https://api.telegram.org/bot';
     private const SEND_MESSAGE = 'sendMessage';
     private const GET_UPDATES = 'getUpdates';
+    private const SEND_DOCUMENT = 'sendDocument';
 
     private string $token;
     private HttpClientInterface $httpClient;
@@ -25,7 +28,7 @@ class TelegramBotClient
     {
         $response = $this->httpClient->request(
             Request::METHOD_GET,
-            self::API . $this->token . '/' . self::GET_UPDATES
+            $this->getUri(self::GET_UPDATES)
         );
 
         return $response->toArray();
@@ -37,9 +40,34 @@ class TelegramBotClient
     ): array {
         $response = $this->httpClient->request(
             Request::METHOD_GET,
-            self::API . $this->token . '/' . self::SEND_MESSAGE . '?chat_id=' . $chatId . '&text=' . $text
+            $this->getUri(self::SEND_MESSAGE) . '?chat_id=' . $chatId . '&text=' . $text
         );
 
         return $response->toArray();
+    }
+
+    public function sendDocument(string $chatId, string $filePath)
+    {
+        $formFields = [
+            'chat_id' => $chatId,
+            'document' => DataPart::fromPath($filePath),
+        ];
+        $formData = new FormDataPart($formFields);
+
+        $response = $this->httpClient->request(
+            Request::METHOD_POST,
+            $this->getUri(self::SEND_DOCUMENT),
+            [
+                'headers' => $formData->getPreparedHeaders()->toArray(),
+                'body' => $formData->bodyToIterable(),
+            ]
+        );
+
+        return $response->toArray();
+    }
+
+    private function getUri(string $telegramMethod): string
+    {
+        return self::API . $this->token . '/' . $telegramMethod;
     }
 }
